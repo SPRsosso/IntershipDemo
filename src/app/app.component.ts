b04e0fa78e9caa55a6cb6661f9d5c42640bf06c9
sossopyc <cypossosmu51c@gmail.com>
2024-04-12 14:21:54 +0200

    Tour of Heroes app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,22 @@
-import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { WishlistItem } from '../shared/models/wishlistItem';
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet, provideRouter } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { WishListComponent } from './wish-list/wish-list.component';
-import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
-import { WishFilterComponent } from './wish-filter/wish-filter.component';
-
-const filters = [
-  (item: WishlistItem) => item,
-  (item: WishlistItem) => !item.isComplete,
-  (item: WishlistItem) => item.isComplete
-]
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    CommonModule, 
-    FormsModule,
-    WishListComponent,
-    AddWishFormComponent,
-    WishFilterComponent
+    RouterOutlet,
+    RouterModule,
+    HeroesComponent,
+    MessagesComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppComponent {
-  items: WishlistItem[] = [
-    new WishlistItem("To learn Angular"),
-    new WishlistItem("Get coffee", true),
-    new WishlistItem("Find grass that cuts itself")
-  ];
-
-  filteredItems = this.items;
-  selectFilter: number = 0;
-  onFilterChange(selectedFilter: number = this.selectFilter) {
-    this.selectFilter = selectedFilter;
-    this.filteredItems = this.items.filter(filters[selectedFilter])
-  }
+  title = 'Tour of Heroes';
 }
